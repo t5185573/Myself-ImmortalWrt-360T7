567d7ebb9586170d29dcda37c5d2d80ca1eb966b
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2023-02-11 18:01:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # 自定义默认网关，后方的192.168.10.1即是可自定义的部分
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 固件版本名称自定义
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu ($time) '/g" package/base-files/files/etc/openwrt_release
+# sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu ($time) '/g" package/base-files/files/etc/openwrt_release
